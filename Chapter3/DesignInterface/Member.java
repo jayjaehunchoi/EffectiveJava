3d1895d514231936aaaa1d1ee1deb1b6e47e96cd
jayjaehunchoigit config --global user.name jayjaehunchoigit config --global user.name jayjaehunchoi <wogns0108@naver.com>
2021-11-29 14:44:10 +0900

    인터페이스 학습내용 추가

---
+++ Chapter3/DesignInterface/Member.java
@@ -0,0 +1,8 @@
+package Chapter3.DesignInterface;
+
+public class Member implements MemberConstants{
+    public static void main(String[] args) {
+        String memberName = Member.MEMBER_NAME;
+
+    }
+}
---
+++ Chapter3/DesignInterface/MemberConstants.java
@@ -0,0 +1,5 @@
+package Chapter3.DesignInterface;
+
+public interface MemberConstants {
+    static final String MEMBER_NAME = "이름";
+}
